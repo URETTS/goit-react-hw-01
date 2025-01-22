2880175b13099f7a41377ffcd9e13935e295600d
URETTS <yurka.b2000@gmail.com>
2025-01-22 18:42:08 +0400

    Initial commit with Profile component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+import Profile from "./components/Profile";
+import userData from "./userData.json";
+
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+};
+
+export default App;
